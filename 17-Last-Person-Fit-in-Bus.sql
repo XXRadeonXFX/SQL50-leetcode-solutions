49f6bbb40bdee00c9f969092c5100beb691b0393
XXRadeonXFX <101211474+XXRadeonXFX@users.noreply.github.com>
2025-11-08 15:32:33 +0530

    Update 17-Last-Person-Fit-in-Bus.sql

--- 17-Last-Person-Fit-in-Bus.sql
+++ 17-Last-Person-Fit-in-Bus.sql
@@ -19,3 +19,26 @@ WHERE sm <= 1000
 WHERE RN =1			
 
 ;
+
+
+
+
+WITH Queue AS (
+    SELECT 5 AS person_id, 'Alice' AS person_name, 250 AS weight, 1 AS turn UNION ALL
+    SELECT 4, 'Bob', 175, 5 UNION ALL
+    SELECT 3, 'Alex', 350, 2 UNION ALL
+    SELECT 6, 'John Cena', 400, 3 UNION ALL
+    SELECT 1, 'Winston', 500, 6 UNION ALL
+    SELECT 2, 'Marie', 200, 4
+)
+
+,CTE AS 
+(
+    SELECT person_name, SUM(weight) OVER(ORDER BY turn ASC) AS sum
+    FROM Queue
+)
+SELECT person_name
+FROM cte
+WHERE sum <= 1000
+ORDER BY sum DESC
+LIMIT 1;
