500a6a1d7e9376f0b1cfc334f9abd7627f4956ed
XXRadeonXFX <101211474+XXRadeonXFX@users.noreply.github.com>
2025-11-09 14:16:26 +0530

    Create Activities table and insert sample data

---
+++ 29-Group-Sold-Products-By-Date.sql
@@ -0,0 +1,21 @@
+USE leetcode;
+
+CREATE TABLE Activities (
+    sell_date DATE,
+    product VARCHAR(100)
+);
+
+
+INSERT INTO Activities (sell_date, product) VALUES
+('2020-05-30', 'Headphone'),
+('2020-06-01', 'Pencil'),
+('2020-06-02', 'Mask'),
+('2020-05-30', 'Basketball'),
+('2020-06-01', 'Bible'),
+('2020-06-02', 'Mask'),
+('2020-05-30', 'T-Shirt');
+
+
+SELECT sell_date , COUNT( DISTINCT product) AS num_sold, GROUP_CONCAT( DISTINCT product ORDER BY product SEPARATOR  ',' ) AS products
+  FROM Activities
+GROUP BY sell_date  ;
