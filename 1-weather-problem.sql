33a13f51e2b76e5829adf91d581fed5a43235f0d
Prince Thakur <101211474+XXRadeonXFX@users.noreply.github.com>
2025-11-20 15:33:26 +0530

    Update 1-weather-problem.sql

--- 1-weather-problem.sql
+++ 1-weather-problem.sql
@@ -1,3 +1,6 @@
+USE leetcode;
+
+DROP TABLE IF EXISTS weather;
 CREATE TABLE Weather (
     id INTEGER PRIMARY KEY,
     recordDate DATE NOT NULL,
@@ -5,6 +8,7 @@ CREATE TABLE Weather (
 );
 
 -- Insert the data
+
 INSERT INTO Weather (id, recordDate, temperature) VALUES
 (1, '2015-01-31', 10),
 (2, '2015-02-01', 25),
@@ -12,18 +16,17 @@ INSERT INTO Weather (id, recordDate, temperature) VALUES
 (4, '2015-02-04', 30);
 
 
-SHOW DATABASES ;
-USE leetcode;
+WITH CTE AS (
+SELECT 
+* , LEAD(recordDate,1) OVER( ORDER BY recordDate ) AS ld_dt
+, LEAD(temperature,1) OVER( ORDER BY recordDate ) AS ld_tmp
+, LEAD(id,1) OVER( ORDER BY recordDate )  AS ld_id
+FROM weather ) 
 
-DELETE FROM Weather WHERE 1=1;
+, CTE1 AS  (
+SELECT CASE WHEN DATEDIFF(ld_dt , recordDate) = 1 AND ld_tmp > temperature THEN ld_id END AS id
+FROM CTE )
 
-TRUNCATE TABLE Weather;
-
-SELECT * FROM Weather;
+SELECT DISTINCT id FROM CTE1 WHERE id IS NOT NULL;
 
 
-    SELECT *,
-        LEAD(temperature, 1) OVER(ORDER BY recordDate) AS next_temp,
-        LEAD(recordDate, 1) OVER(ORDER BY recordDate) AS next_date,
-        LEAD(id, 1) OVER(ORDER BY recordDate) AS next_id
-    FROM Weather
